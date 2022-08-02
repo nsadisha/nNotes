7abafc4df9573c910b19b5c0ad3fdf3be1488553
Sadisha Nimsara <nsadisha@gmail.com>
2022-08-02 17:51:39 +0530

    Initial Commit

---
+++ nNotes/nNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  nNotesApp.swift
+//  nNotes
+//
+//  Created by Sadisha Nimsara on 2022-08-02.
+//
+
+import SwiftUI
+
+@main
+struct nNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
